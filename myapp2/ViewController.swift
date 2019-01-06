cece842e677077a04acfa14d5340e570b41400d0
connieanntan <connieanntan@gmail.com>
2019-01-06 20:59:44 +0800

    Initial Commit

---
+++ myapp2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  myapp2
+//
+//  Created by connieanntan on 2019/1/6.
+//  Copyright © 2019 connieanntan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
